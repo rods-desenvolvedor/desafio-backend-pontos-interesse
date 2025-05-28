c17d9e66c094e3ae5f3fed1dfe0af45b12df7759
rods <rods.desenvolvedor@gmail.com>
2025-05-28 15:23:40 -0400

    feat: add função para buscar servicos por localização e função para calcular distancia entre pontos

--- src/main/java/com/desafio/poi/service/PontoInteresseService.java
+++ src/main/java/com/desafio/poi/service/PontoInteresseService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.desafio.poi.controller.dtos.PontoInteresseRequestDto;
 import com.desafio.poi.controller.dtos.PontoInteresseResponseDto;
+import com.desafio.poi.controller.dtos.PontosPorProximidadeRequestDto;
 import com.desafio.poi.entity.PontoInteresse;
 import com.desafio.poi.repository.PontoInteresseRepository;
 
@@ -35,5 +36,22 @@ public class PontoInteresseService {
         List<PontoInteresse> pontosInteresse = pontoInteresseRepository.findAll();
 
         return pontosInteresse.stream().map(PontoInteresseResponseDto::new).toList();
+    }
+
+    public List<PontoInteresseResponseDto> listarPontosPorProximidade(PontosPorProximidadeRequestDto pontosPorProximidadeRequestDto)
+    {
+        List<PontoInteresse> pontosInteresse = pontoInteresseRepository.findAll();
+
+        List<PontoInteresse> pontosPorProximidade = pontosInteresse.stream()
+        .filter(ponto -> calcularDistancia(ponto.getX(), pontosPorProximidadeRequestDto.x(),
+        ponto.getY(),pontosPorProximidadeRequestDto.y()) <= pontosPorProximidadeRequestDto.distMaxima()).toList();
+
+        return pontosPorProximidade.stream().map(PontoInteresseResponseDto::new).toList();
+    }
+
+    public Double calcularDistancia(int x1, int x2, int y1, int y2)
+    {
+        Double dist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        return dist;
     }
 }
