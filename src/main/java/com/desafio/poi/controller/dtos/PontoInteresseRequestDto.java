17d6118e470e9909a44df5167e036ee49cb8ad2c
rods <rods.desenvolvedor@gmail.com>
2025-05-28 10:32:18 -0400

    feat: add validações no dto request

--- src/main/java/com/desafio/poi/controller/dtos/PontoInteresseRequestDto.java
+++ src/main/java/com/desafio/poi/controller/dtos/PontoInteresseRequestDto.java
@@ -1,5 +1,20 @@
 package com.desafio.poi.controller.dtos;
 
-public record PontoInteresseRequestDto(String nome, int x, int y) {
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record PontoInteresseRequestDto(
+    
+    @NotEmpty(message = "O nome do Ponto de Interesse é obrigatório")
+    String nome,
+
+    @PositiveOrZero(message = "O valor da coordenada não pode ser negativo")
+    @NotNull(message = "A coordenada X é obrigatória para cadastrar um novo ponto de interesse")
+    int x,
+
+    @PositiveOrZero(message = "O valor da coordenada não pode ser negativo")
+    @NotNull(message = "A coordenada Y é obrigatória para cadastrar um novo ponto de interesse")
+    int y) {
     
 }
