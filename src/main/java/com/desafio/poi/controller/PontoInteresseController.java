ac06802dd69d13023a2573aec2e7f6786929601e
rods <rods.desenvolvedor@gmail.com>
2025-05-28 11:33:45 -0400

    feat: add controller do ponto interesse - endpoint cadastrar add

---
+++ src/main/java/com/desafio/poi/controller/PontoInteresseController.java
@@ -0,0 +1,32 @@
+package com.desafio.poi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.desafio.poi.controller.dtos.PontoInteresseRequestDto;
+import com.desafio.poi.controller.dtos.PontoInteresseResponseDto;
+import com.desafio.poi.service.PontoInteresseService;
+
+@RestController
+@RequestMapping("/api/ponto-interesse")
+public class PontoInteresseController {
+
+    private final PontoInteresseService pontoInteresseService;
+
+    public PontoInteresseController(PontoInteresseService pontoInteresseService)
+    {
+        this.pontoInteresseService = pontoInteresseService;
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<PontoInteresseResponseDto> cadastrarPontoInteresse(@RequestBody PontoInteresseRequestDto pontoInteresseRequestDto)
+    {
+        PontoInteresseResponseDto pontoInteresseResponseDto = pontoInteresseService.cadastrarPontoInteresse(pontoInteresseRequestDto);
+
+        return ResponseEntity.ok().body(pontoInteresseResponseDto);
+    }
+    
+}
